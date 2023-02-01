2a5fc064c4f5ef483801a654398a8a6d709abdd4
Bo Tran <100829506+s3954173@users.noreply.github.com>
2023-02-01 13:13:37 +1100

    Cloud Translate Sample Code

---
+++ Cloud Translate/translate_test.py
@@ -0,0 +1,5 @@
+import basic_sample as ct
+input_text = input("Hello, what is the phrase you'd like me to translate? ")
+translated_text = ct.translate_text("zh", input_text)
+print(translated_text)
+
