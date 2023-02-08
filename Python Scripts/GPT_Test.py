8c4d9d2f1fa889d0007576f5886f3a8ade178c5a
SashaBrooke <123516043+SashaBrooke@users.noreply.github.com>
2023-02-08 12:22:53 +1100

    Added splitting functionality and info logging.

--- Python Scripts/GPT_Test.py
+++ Python Scripts/GPT_Test.py
@@ -2,7 +2,6 @@ import openai
 import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-# print(os.getenv("OPENAI_API_KEY"))
 
 def generate_response(prompt):
     model_engine = "text-davinci-003"
@@ -20,9 +19,39 @@ def generate_response(prompt):
     message = completions.choices[0].text
     return message.strip()
 
+def sentenceCheck(inputSentence):
+    aiSentenceCheck = generate_response("Is " + splitResponse[0] + " a sentence?")
+    charList = list(aiSentenceCheck)
+    if (charList[0]+charList[1]).lower() == "no":
+        isSentence = False
+    elif (charList[0]+charList[1]+charList[2]).lower() == "yes":
+        isSentence = True
+    return isSentence
+
 while True:
     user_input = input("User: ")
-    if user_input == "exit":
+    if user_input.lower() == "exit":
         break
     response = generate_response(user_input)
-    print("Chatbot:", response)
+    
+
+
+
+    splitResponse = response.split("\n", maxsplit=1)
+    if len(splitResponse) == 1:
+        print(1)
+
+        print("Chatbot:", response)
+    elif len(splitResponse) == 2:
+        print(2)
+
+        isSentence = sentenceCheck(splitResponse[0])
+        if isSentence == True:
+            print("isSentence=True")
+
+            print("Chatbot:", response)
+        elif isSentence == False:
+            print("isSentence=False")
+
+            print("[ " + splitResponse[0] + " ]")
+            print("[ " + splitResponse[1] + " ]")
