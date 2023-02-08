6636691350beb36cc8daaf983a3cbf7b74dffd1c
Bo Tran <100829506+s3954173@users.noreply.github.com>
2023-02-08 16:07:50 +1100

    Issues with ChatGPT not working on gcloud

--- main.py
+++ main.py
@@ -13,32 +13,49 @@ def callgpt(request):
        Response object using `make_response`
        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
    """
-   request_json = request.get_json(silent=True)
-   request_args = request.args
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
-   if request_json and 'message' in request_json:
-       openai.api.key = request_json['openai_api_key']
+    if request_json and 'message' in request_json:
+       openai_key = request_json['openai_api_key']
        message = request_json['message']
-   elif request_args and 'message' in request_args:
-       openai.api.key = request_args['openai_api_key']
+    elif request_args and 'message' in request_args:
+       openai_key = request_args['openai_api_key']
        message = request_args['message']
-   else:
+    else:
        message = "I'm sorry could you repeat that again?"
-   translated_message = translate(message, 'japanese')
-   return translated_message
+       
+    # GPT Translate
+    openai.api.key = openai_key
+    language = "japanese"		
+    model_engine = "test-davinci-003"
+    prompt = f"Translate {message} into {language}."
 
-def translate(phrase, language):
+    completions = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            )
+
+    translated_message = completions.choices[0].text
+    return translated_message
+
+"""def translate(phrase, language, openai_key):
+    
     model_engine = "test-davinci-003"
     prompt = (f"Translate {phrase} into {language}")
 
     completions = openai.Completion.create(
-             engine=model_engine,
-             prompt=prompt,
-             max_tokens=1024,
-             n=1,
-             stop=None,
-             temperature=0.5,
-             )
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            )
 
     message = completions.choices[0].text
-    return message.strip()
+    return message.strip() """
