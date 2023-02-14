54be11777d815aeacbab84753c6c23b9720c7b65
Bo Tran <100829506+s3954173@users.noreply.github.com>
2023-02-14 16:33:33 +1100

    callgpt takes language parameter to translate

--- main.py
+++ main.py
@@ -19,16 +19,18 @@ def callgpt(request):
     if request_json and 'message' in request_json:
        openai_key = request_json['key']
        message = request_json['message']
+       language = request_json['language']
     elif request_args and 'message' in request_args:
        openai_key = request_args['key']
        message = request_args['message']
+       language = request_args['language']
     else:
        message = "I'm sorry could you repeat that again?"
        openai_key = request_args['key']
+       language = "japanese"
        
     # GPT Translate
-    openai.api_key = openai_key
-    language = "japanese"		
+    openai.api_key = openai_key		
     model_engine = "text-davinci-003"
     prompt = f"Translate {message} into {language}."
 
@@ -44,19 +46,3 @@ def callgpt(request):
     translated_message = completions.choices[0].text
     return translated_message
 
-"""def translate(phrase, language, openai_key):
-    
-    model_engine = "test-davinci-003"
-    prompt = (f"Translate {phrase} into {language}")
-
-    completions = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-            )
-
-    message = completions.choices[0].text
-    return message.strip() """
