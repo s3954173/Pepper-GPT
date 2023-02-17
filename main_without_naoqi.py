d9a4bb670e4f71afcd94f6f25542278bb2e9ec8d
SashaBrooke <123516043+SashaBrooke@users.noreply.github.com>
2023-02-17 18:24:36 +1100

    Speech recognition functionality completed, waiting on Gcloud section.

--- main_without_naoqi.py
+++ main_without_naoqi.py
@@ -1,20 +1,19 @@
 import os
 import urllib
+import urllib.request
 # import urllib2
 import json
-import time
+import speech_recognition as sr
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-############################################################################################################################
-
 #TODO Pepper Listening
-import speech_recognition as sr
-
 # Initialize recognizer
 r = sr.Recognizer()
 
-mic_name = 'Microphone (Arctis 5 Chat)'
+mic_name = 'Microphone (Arctis 5 Chat)' # for sasha system
+# mic_name = 'XXXXXXXXXXXXXX' # for bo system
+
 mic = sr.Microphone(device_index=sr.Microphone.list_microphone_names().index(mic_name))
 
 # Start listening
@@ -36,31 +35,27 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print("Could not request results; {0}".format(e))
 
-############################################################################################################################
-
-
-
-
-
-# #TODO Run gcloud function from propmt
-# # To delete test variables language and message
-# language = "Chinese"
+######################### For Bo to work on this weekend ###########################
+#TODO Run gcloud function from propmt 
+# To delete test variables language and message
+language = "Chinese"
+message = text
 # message = "Test message"
 
-# url = "https://callgpt-gemqjtz7eq-ts.a.run.app"
-# values = {"api_key": api_key,
-#         "language": language,
-#         "message": message}
-# headers = {"Authorization":"bearer $(gcloud auth print-identity-token)",
-#         "Content-Type": "application/json"}
+url = "https://callgpt-gemqjtz7eq-ts.a.run.app"
+values = {"api_key": api_key,
+        "language": language,
+        "message": message}
+headers = {"Authorization":"bearer $(gcloud auth print-identity-token)",
+        "Content-Type": "application/json"}
 
-# data = json.dumps(values, indent=len(values))
-# req = urllib.Request(url, data, headers)
-# url_response = urllib.urlopen(req)
-# # req = urllib2.Request(url, data, headers)
-# # url_response = urllib2.urlopen(req)
-# translated_message = url_response.read()
-# # print(translated_message)
+data = json.dumps(values, indent=len(values))
+req = urllib.request.Request(url, data, headers)
+url_response = urllib.request.urlopen(req)
+# req = urllib2.Request(url, data, headers)
+# url_response = urllib2.urlopen(req)
+translated_message = url_response.read()
+# print(translated_message)
 
-# #TODO Say to user
-# print(message + " translated into " + language + " is " + translated_message)
+#TODO Say to user
+print(message + " translated into " + language + " is " + translated_message)
