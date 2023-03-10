73a9e4015cef7279b9d812c9749cf09088719c5b
Bo Tran <100829506+s3954173@users.noreply.github.com>
2023-03-10 13:57:01 +1100

    SR testing and experimenting failure

--- gcloud.py
+++ gcloud.py
@@ -4,21 +4,19 @@ import json
 
 class GPTfunc:
     # Variables
-    
-    # Constructor
     def __init__(self, api_key):
         self.api_key = api_key
+        self.headers = {"Authorization":"bearer $(gcloud auth print-identity-token)", "Content-Type": "application/json"}
 
     # Methods
     def translate(self, url, language, message):
-        headers = {"Authorization":"bearer $(gcloud auth print-identity-token)", "Content-Type": "application/json"}
         values = {"api_key": self.api_key,
                 "language": language,
                 "message": message}
         data = json.dumps(values, indent=len(values)) #Convert dictionary to JSON
-       
+
         # Call gcloud function
-        req = urllib2.Request(url, data, headers) 
+        req = urllib2.Request(url, data, self.headers) 
         url_response = urllib2.urlopen(req)
 
         # Return translated message
